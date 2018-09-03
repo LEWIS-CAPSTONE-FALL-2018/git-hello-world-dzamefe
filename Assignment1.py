4117869893db452696001646e682ae5007d9bee4
Richard Dzamefe <dzamefe@gmail.com>
2018-09-02 19:35:22 -0700

    This is my first gitHub transaction, adding 2 files to gitHub

---
+++ Assignment1.py
@@ -0,0 +1,17 @@
+"""
+Name: Richard Dzamefe
+
+Class: CPSC 49200-001
+
+Date: 2 Sep 2018
+
+Assignment 1
+
+Description: Print out "Hello World" in any programming language
+
+"""
+
+
+# printing out "Hello , world" to the screen using the Python programming language
+
+print("Hello, world")
